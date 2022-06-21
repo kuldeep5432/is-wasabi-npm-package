ffaa28903fd565c83464032ea3d1bba0dc601f36
Kuldeep Kumar <kuldeep.kumar@zendesk.com>
2022-06-21 17:24:51 +0100

    added another function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function isWasabi(string) {
-    return string == "wasabi"
+let creator = "wabisabi5432"
+
+function getLength(string) {
+    // return string == "wasabi"
+    return string.length
 }
 
-module.exports = isWasabi
+function isWasabi(string) {
+    return string == "wasabi"
+    // return string.length
+}
+
+module.exports.creator = creator
+module.exports.getLength = getLength
+module.exports.isWasabi = isWasabi
